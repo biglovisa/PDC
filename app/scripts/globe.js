4257da66af03dd71c800da42db8c0d1ff4e2e4e1
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-20 22:03:19 -0600

    minor changes

--- app/scripts/globe.js
+++ app/scripts/globe.js
@@ -5,7 +5,6 @@ export default React.createClass ({
   render: function() {
       return (
       <div className="globe col-md-4">
-        <img src="./public/images/globe.jpg" />
       </div>
     );
   },
