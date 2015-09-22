3c3e6e2e9915dca4480b266569a99ac38768fd6a
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-22 12:04:20 -0600

    changes to index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('app/index');
+  response.render('dist/index');
 });
 
 app.listen(app.get('port'), function() {
