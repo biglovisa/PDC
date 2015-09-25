a3abf754f33f40bbe72824a29f5697cc9d60ae5b
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-25 12:19:31 -0600

    heroku f you

--- app/scripts/util/api.js
+++ app/scripts/util/api.js
@@ -2,9 +2,7 @@ function getCountryData(countryCode, dataType) {
   return $.ajax({
     url: `https://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
     type: 'GET',
-    dataType: 'jsonp',
-    jsonp: 'prefix',
-    jsonpCallback: 'jquery_'+(new Date).getTime(),
+    dataType: 'json',
     headers: {'Access-Control-Allow-Origin': 'https://obscure-retreat-5519.herokuapp.com/'},
   });
 }
