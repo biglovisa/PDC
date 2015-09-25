ca052e5078166445875dff54aad437926effe861
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-25 11:57:05 -0600

    add https to api call

--- app/scripts/util/api.js
+++ app/scripts/util/api.js
@@ -1,6 +1,6 @@
 function getCountryData(countryCode, dataType) {
   return $.ajax({
-    url: `http://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
+    url: `https://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
     type: 'GET',
     dataType: 'jsonp',
     jsonp: 'prefix',
