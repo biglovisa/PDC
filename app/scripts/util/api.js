52d09889f5e09374b6b02f49bff58dfe5b27b5fb
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-27 17:06:19 -0600

    remove http from ajax call

--- app/scripts/util/api.js
+++ app/scripts/util/api.js
@@ -1,6 +1,6 @@
 function getCountryData(countryCode, dataType) {
   return $.ajax({
-    url: `https://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
+    url: `http://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
     type: 'GET',
     dataType: 'json',
     headers: {'Access-Control-Allow-Origin': 'https://obscure-retreat-5519.herokuapp.com/'},
