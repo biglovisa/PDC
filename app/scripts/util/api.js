09ad4bee4524c5cb6cf844c7c4aa84e4e6800dfd
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-27 17:15:09 -0600

    change https to http

--- app/scripts/util/api.js
+++ app/scripts/util/api.js
@@ -3,7 +3,7 @@ function getCountryData(countryCode, dataType) {
     url: `http://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
     type: 'GET',
     dataType: 'json',
-    headers: {'Access-Control-Allow-Origin': 'https://obscure-retreat-5519.herokuapp.com/'},
+    headers: {'Access-Control-Allow-Origin': 'http://obscure-retreat-5519.herokuapp.com/'},
   });
 }
 
