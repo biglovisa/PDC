6da6396a67c99c05f536809f3dfd5479892db6ab
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-28 13:02:39 -0600

    change heroku url to the new name

--- app/scripts/util/Api.js
+++ app/scripts/util/Api.js
@@ -1,11 +1,11 @@
 function getCountryData(countryCode, dataType) {
   return $.ajax({
-    url: `https://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
+    url: `http://api.worldbank.org/countries/${countryCode}/indicators/${dataType}?per_page=56&format=jsonP`,
     type: 'GET',
     dataType: 'jsonp',
     jsonp: 'prefix',
     jsonpCallback: 'jquery_'+(new Date).getTime(),
-    headers: { 'Access-Control-Allow-Origin': 'https://obscure-retreat-5519.herokuapp.com' }
+    headers: { 'Access-Control-Allow-Origin': 'https://public-data-compiler.herokuapp.com' }
   });
 }
 
