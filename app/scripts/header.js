d1c55fbaa3f2e3ffedd36168b4255e34d0a555da
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-27 23:05:25 -0600

    Add chart selections in header
    * remove component chart type
    * add 'gdp map' and 'compare countries' as options in nav bar
    * on click 'gdp map' or 'compare countries', render content accordingly

--- app/scripts/header.js
+++ app/scripts/header.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React            from 'react';
 
 export default React.createClass({
+  handleClick: function(value) {
+    this.props.handleLinkClick(value);
+  },
   render: function(){
     return (
       <div className='header'>
@@ -8,31 +11,22 @@ export default React.createClass({
           <h3>
           Public Data Compiler
           </h3>
-          <RepositoryLink />
+          <a href='https://github.com/applegrain/PDC'className='repository-link'
+          >
+          Repository
+          </a>
+          <a className='repository-link' onClick={this.handleClick.bind(this, 'geo')}
+          >
+          GDP Map
+          </a>
+          <a className='repository-link' onClick={this.handleClick.bind(this, 'two')}
+          >
+          Compare Countries
+          </a>
         </div>
 
-        <Description />
+        <p>Query World Bank Data to compare global statistics</p>
       </div>
-    );
-  },
-});
-
-var RepositoryLink = React.createClass({
-  render: function(){
-    return (
-      <a href='https://github.com/applegrain/PDC'
-         className='repository-link'
-      >
-      Repository
-      </a>
-    );
-  },
-});
-
-var Description = React.createClass({
-  render: function(){
-    return (
-      <p>Query World Bank Data to compare global statistics</p>
     );
   },
 });
