ab9ec270143d257561761d56536ef77328781644
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-28 21:47:48 -0600

    Add key events to country selection
    * add package 'classnames'
    * add key events (arrow down, arrow up, enter) to navigate the dropdown

--- app/scripts/SelectCountry.js
+++ app/scripts/SelectCountry.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React      from 'react';
+import classNames from 'classnames';
 
 export default React.createClass({
   getInitialState: function() {
-    return {inputText: '', showDropdown: false};
+    return {inputText: '', activeListItem: null, filteredListItems: [], showDropdown: false, hoveredItem: null};
   },
   handleSearch: function(e) {
-    this.setState({showDropdown: true, inputText: e.target.value});
+    var inputText = e.target.value;
+    this.setState({showDropdown: true, inputText: inputText});
+
+    if (inputText) {
+      var pattern = inputText.toLowerCase().split(" ").map(function(el) {
+        return "(" + el + ".*)"
+      }).join(" ");
+
+      var listItems = this.props.countries.filter(v => {
+        return v.toLowerCase().match(new RegExp(pattern));
+      });
+
+      this.setState({filteredListItems: listItems});
+    }
   },
   selectValue: function(item) {
     this.setState({showDropdown: false, inputText: item});
@@ -14,36 +28,52 @@ export default React.createClass({
 
     this.setState({inputText: ''});
   },
-  render: function() {
-    var dropdown = null;
-
-    if (this.state.showDropdown && this.state.inputText){
-      var pattern = this.state.inputText.toLowerCase().split(" ").map(function(el){
-        return "(" + el + ".*)"
-      }).join(" ");
-
-      var listItems = this.props.countries.filter(v => {
-        return v.toLowerCase().match(new RegExp(pattern));
-      }).map(item => {
-        return (
-          <li
-            onClick={this.selectValue.bind(this, item)}
-            key={item} >
-            {item}
-          </li>
-        );
-      });
-
-      dropdown = (
-        <ul
-          style={{overflow: 'auto'}}
-          className='dropdown'
-        >
-          {listItems}
-        </ul>
-      );
+  handleKeyDown: function(e) {
+    switch (e.key) {
+      case 'ArrowDown':
+        // var index = this.state.activeListItem === null ? 0 : this.state.activeListItem + 1;
+        this.setState({activeListItem: this.incrementActiveLi()});
+        break;
+      case 'ArrowUp':
+        var index = this.state.activeListItem === null ? 0 : this.state.activeListItem - 1;
+        this.setState({activeListItem: index});
+        break;
+      case 'Enter':
+        var country = this.state.filteredListItems[this.state.activeListItem]
+        this.setState({activeListItem: null});
+        this.selectValue(country);
+        break;
+    }
+  },
+  incrementActiveLi() {
+    return this.state.activeListItem === null ? 0 : this.state.activeListItem + 1;
+  },
+  renderDropdown: function() {
+    if (!this.state.showDropdown || !this.state.inputText) {
+      return null;
     }
 
+    var listItems = this.state.filteredListItems.map((item, index) => {
+      return (
+        <li
+          className={classNames({hoverLi: this.state.activeListItem === index})}
+          onClick={this.selectValue.bind(this, item)}
+          key={item}>
+          {item}
+        </li>
+      );
+    });
+
+    return (
+      <ul
+        style={{overflow: 'auto'}}
+        className='dropdown'
+      >
+        {listItems}
+      </ul>
+    );
+  },
+  render: function() {
     return (
       <div className='search-form'>
         <input
@@ -52,9 +82,10 @@ export default React.createClass({
           value={this.state.inputText}
           placeholder='Search for a country...'
           onChange={this.handleSearch}
+          onKeyDown={this.handleKeyDown}
         />
         <div>
-          {dropdown}
+          {this.renderDropdown()}
         </div>
       </div>
     );
