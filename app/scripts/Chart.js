3e5dfba1a333fe7ee4fa8134b6f25a177431fb87
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-30 21:03:21 -0600

    Message displaying which countries are missing data
    * displays which countries that don't have any datapoints for
      given data option
    
    * BUGS:
      * it's possible to select empty string. empty button renders
      * if a country with no data points is selected, subsequent
        countries selected with datapoints won't render and the chart closes

--- app/scripts/Chart.js
+++ app/scripts/Chart.js
@@ -1,17 +1,18 @@
-import React        from 'react';
-import ChartOptions from './constants/ChartOptions';
-import NanTruncated from './functions/NanTruncated'
-import Chart        from 'c3-react';
+import React         from 'react';
+import ChartOptions  from './constants/ChartOptions';
+import IfMissingData from './functions/IfMissingData';
+import NanTruncated  from './functions/NanTruncated';
+import Chart         from 'c3-react';
 
 export default React.createClass({
   render: function() {
     var values = this.props.values;
-
     var trimmedCountries = values.map(function(country) { return {key: country.key, values: NanTruncated(country.values)} });
-
     return (
       <div className='chart pull-left' key='chart'>
-        <h5 className="details">{this.props.details}</h5>
+        <h5 className='details'>{this.props.details}</h5>
+        {IfMissingData(trimmedCountries)}
+
         <Chart data={trimmedCountries} type='lineBar' options={ChartOptions} />
       </div>
     );
---
+++ app/scripts/functions/IfMissingData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function(countries) {
+  var missing = [];
+
+  for (var i = 0; i < countries.length; i++ ) {
+    if (countries[i].values.length === 0) {
+      missing.push(countries[i].key);
+    }
+  }
+
+  return missing.length === 0 ?  "" : <h6 className='details'>Missing data for: {missing.join(", ")}</h6>;
+}
